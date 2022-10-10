2b90345137108f94dafb76db9ba5712d7b4a1a57
Nikita Davidov <n.davidov@navigine.com>
2022-10-10 16:17:01 +0300

    - update UserSession

--- NavigineDemo/app/src/main/java/com/navigine/navigine/demo/models/UserSession.java
+++ NavigineDemo/app/src/main/java/com/navigine/navigine/demo/models/UserSession.java
@@ -5,7 +5,7 @@ import com.navigine.navigine.demo.BuildConfig;
 public class UserSession {
 
     public static String USER_HASH       = BuildConfig.DEFAULT_USER_HASH;
-    public static String USER_NAME       = "default";
+    public static String USER_NAME       = "user";
     public static String USER_COMPANY    = "company";
     public static String USER_EMAIL      = "default@mail.com";
     public static String USER_AVATAR_URL = "";
