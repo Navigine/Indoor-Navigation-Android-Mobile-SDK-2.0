d985d9f85bac0db854e3500444331c6694d00797
Nikita Davidov <n.davidov@navigine.com>
2022-10-10 18:44:15 +0300

    - update MainActivity

--- NavigineDemo/app/src/main/java/com/navigine/navigine/demo/ui/activities/MainActivity.java
+++ NavigineDemo/app/src/main/java/com/navigine/navigine/demo/ui/activities/MainActivity.java
@@ -1,20 +1,13 @@
 package com.navigine.navigine.demo.ui.activities;
 
 
-import static com.navigine.navigine.demo.utils.Constants.DL_QUERY_SERVER;
-import static com.navigine.navigine.demo.utils.Constants.DL_QUERY_USERHASH;
-import static com.navigine.navigine.demo.utils.Constants.TAG;
-
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.navigine.navigine.demo.R;
 import com.navigine.navigine.demo.models.BeaconMock;
-import com.navigine.navigine.demo.models.UserSession;
 import com.navigine.navigine.demo.ui.custom.navigation.SavedBottomNavigationView;
 import com.navigine.navigine.demo.utils.NavigineSdkManager;
 import com.navigine.navigine.demo.viewmodel.SharedViewModel;
@@ -33,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        handleDeepLink();
         setContentView(R.layout.activity_main);
         initViewModel();
         initNavigationView();
@@ -47,24 +39,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         finishAffinity();
-    }
-
-    private void handleDeepLink() {
-        Uri data = getIntent().getData();
-        if (data != null) {
-            try {
-                String locationServer = data.getQueryParameter(DL_QUERY_SERVER);
-                String userHash = data.getQueryParameter(DL_QUERY_USERHASH);
-
-                UserSession.LOCATION_SERVER = locationServer;
-                UserSession.USER_HASH = userHash;
-
-                NavigineSdkManager.initializeSdk();
-
-            } catch (UnsupportedOperationException | NullPointerException e) {
-                Log.e(TAG, getString(R.string.err_deep_link_parse));
-            }
-        }
     }
 
     private void initViewModel() {
